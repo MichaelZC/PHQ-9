0c1beb427b0af2ee42e54c2160450000c452173b
Michael Cheng <michael.zhang.cheng@gmail.com>
2017-01-12 09:33:04 -0500

    start working on text component

--- core/app.js
+++ core/app.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import Text from './components/text';
 
+const textProps = {
+  text: 'Hello World!',
+};
+
 export default () => (
   <div>
-    <Text />
+    <Text {...textProps} />
   </div>
 );
--- core/components/text.js
+++ core/components/text.js
@@ -2,6 +2,6 @@ import React from 'react';
 
 export default props => (
   <div>
-    ${props.text}
+    {props.text}
   </div>
 );
