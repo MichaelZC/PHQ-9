ee82bdc173a20c97cd353c773266653d5a223c8f
Michael Cheng <michael.zhang.cheng@gmail.com>
2017-01-12 09:50:03 -0500

    refine style of choice

--- core/components/choice.js
+++ core/components/choice.js
@@ -2,8 +2,11 @@ import React from 'react';
 
 const choiceContainerStyle = {
   padding: '1em',
-  backgroundColor: 'red',
+  backgroundColor: '#E8EAF6',
   flex: 1,
+  margin: '1em',
+  border: '0.5px solid #3F51B5',
+  textAlign: 'center',
 };
 
 export default props => (
