c2a28371799a2a80f0a1e1f714f06701ba8c6b68
Michael Cheng <michael.zhang.cheng@gmail.com>
2017-01-12 18:18:30 -0500

    you idiot, 0 is fasly

--- core/containers/question.js
+++ core/containers/question.js
@@ -4,7 +4,8 @@ import Question from '../components/question';
 const mapStateToProps = (state, ownProps) => {
   return {
     // only show if previous question was answered or we are first question
-    visible: ownProps.questionIndex === 0 || !!state.responses[ownProps.questionIndex - 1],
+    visible: ownProps.questionIndex === 0 ||
+      state.responses[ownProps.questionIndex - 1] !== undefined,
   };
 };
 
