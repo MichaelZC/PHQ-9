93aaf58a168cd7595349da9f242e93c09fa0153c
Michael Cheng <michael.zhang.cheng@gmail.com>
2017-01-12 19:25:20 -0500

    some ux enhancements to way questions are displayed

--- core/components/question.js
+++ core/components/question.js
@@ -4,26 +4,38 @@ import Prompt from '../components/prompt';
 import Button from '../containers/button';
 import Row from '../components/row';
 
-const Question = ({ questionIndex, visible }) => {
-  if (!visible) {
-    return <div />;
+class Question extends React.Component {
+  componentDidUpdate() {
+    const { visible } = this.props;
+    if (visible) {
+      window.scrollTo(0,document.body.scrollHeight); //eslint-disable-line
+    }
   }
-  return (
-    <div>
-      <Prompt text={TestConstants.PROMPTS[questionIndex]} />
-      <Row>
-        {TestConstants.RESPONSES.map(
-          (text, i) => <Button
-            key={i} 
-            answerIndex={i}
-            questionIndex={questionIndex}
-            text={text}
-          />,
-        )}
-      </Row>
-    </div>
-  );
-};
+  render() {
+    const { visible, questionIndex } = this.props;
+    return (
+      <div
+        style={{
+          height: visible ? 'auto' : '0',
+          opacity: visible ? '1' : '0',
+          transition: '0.75s',
+        }}
+      >
+        <Prompt text={TestConstants.PROMPTS[questionIndex]} />
+        <Row>
+          {TestConstants.RESPONSES.map(
+            (text, i) => <Button
+              key={i}
+              answerIndex={i}
+              questionIndex={questionIndex}
+              text={text}
+            />,
+          )}
+        </Row>
+      </div>
+    );
+  }
+}
 
 Question.propTypes = {
   questionIndex: React.PropTypes.number.isRequired,
